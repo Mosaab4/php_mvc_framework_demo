51f7803c164e4db0695f2b6402c5ba32dedbea63
Mosaab <musab.ehab@gmail.com>
2021-08-26 20:13:03 +0200

    implement session flash messages

--- Controllers/AuthController.php
+++ Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controllers;
 
+use App\Core\Application;
 use App\Core\Controller;
 use App\Core\Request;
 use App\Models\User;
@@ -24,7 +25,9 @@ class AuthController extends Controller
             $user->loadData($request->getBody());
 
             if ($user->validate() && $user->save()) {
-                return 'Success';
+                Application::$app->session->setFlash('success' , 'Thanks for registering');
+                Application::$app->response->redirect('/');
+                exit;
             }
 
 
